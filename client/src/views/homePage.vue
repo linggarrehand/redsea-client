7c078a7f9442849384f8f89261a729d63bf3e6e6
linggarrehand <linggarrehand@gmail.com>
2023-03-08 08:07:34 +0700

    feat: create home page and product card

--- client/src/views/homePage.vue
+++ client/src/views/homePage.vue
@@ -1,11 +1,30 @@
 <script>
+import productCard from '../components/productCard.vue'
+import { mapActions, mapState } from 'pinia'
+import { useProductStore } from '../stores/product'
 export default {
-    name: "homePage"
+  name: 'homePage',
+  components: {
+    productCard
+  },
+  methods: {
+    ...mapActions(useProductStore, ['fetchProducts'])
+  },
+  computed: {
+    ...mapState(useProductStore, ['products'])
+  },
+  created() {
+    this.fetchProducts()
+  }
 }
 </script>
 
 <template>
-    <div>
-        <h1>Home Page</h1>
+  <section style="background-color: #eee">
+    <div class="container py-5">
+      <div class="row">
+        <productCard v-for="product in products" :key="product.id" :product="product" />
+      </div>
     </div>
+  </section>
 </template>
